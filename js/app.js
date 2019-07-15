14b92c36729cdcd9f40e637fd65ca5d1256ca37f
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-07-15 18:38:09 +0500

    developing portfolio website half complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
-    $('.menu').click(function(){
-        $('ul').toggleClass('active')
-    })
-})
+AOS.init();
+
+console.log(window.pageYOffset)
+
+var previousScrollPos = window.pageYOffset;
+console.log(previousScrollPos)
+window.onscroll = function() {
+    var currentScroll = window.pageYOffset;
+    if(previousScrollPos > currentScroll ) {
+        document.getElementById("navbar").style.top = "0"
+    }
+    else{
+        document.getElementById("navbar").style.top = "-56px"
+    }
+    // console.log(previousScrollPos)
+    // console.log(currentScroll)
+    // console.log(previousScrollPos = currentScroll)
+    previousScrollPos = currentScroll
+}
+
+var headerImage = document.getElementsByClassName('header-img');
+new simpleParallax(headerImage, {
+	scale: 1.4
+});
